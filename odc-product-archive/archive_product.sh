681a733d6dc0c81747b688f36691bf87c2539ccf
Indiphile Ngqambuza <ingqambuza@gmail.com>
2022-12-14 11:57:02 +0200

    testpy2

--- odc-product-archive/archive_product.sh
+++ odc-product-archive/archive_product.sh
@@ -2,13 +2,13 @@
 
 
 datacube system check
-datacube product list
+#datacube product list
 #for local test
 #eval "$(conda shell.bash hook)"
 #conda activate odc_env
 
 echo "PRODUCT NAME"
-read ${PRODUCT_NAME}
+read PRODUCT_NAME
 
 
 python3 archive_odc_product.py ${PRODUCT_NAME}
